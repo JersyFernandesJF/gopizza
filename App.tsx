53fc4a4444e540a08ba120b03a7d57dd5bd44fe7
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2022-07-15 03:07:01 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from '@src/theme'
+
+import { SignIn } from '@src/screens/SignIn';
+
+export default function App() {
+
+    const [ fontsLoaded ] = useFonts({
+
+      DMSans_400Regular,
+      DMSerifDisplay_400Regular
+
+    })
+    if(!fontsLoaded) {
+      return <AppLoading/>
+    }
+
+  return (
+   <ThemeProvider theme={theme}>
+      <SignIn/>
+   </ThemeProvider>
+  );
+}
+
+
